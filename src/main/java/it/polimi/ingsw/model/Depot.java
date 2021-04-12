df8fde787eeadd3a6f4e3e6d7f19133ac641fa96
Alessandra <alessandra.atria@mail.polimi.it>
2021-04-12 20:48:18 +0200

    Depot class + test

---
+++ src/main/java/it/polimi/ingsw/model/Depot.java
@@ -0,0 +1,61 @@
+package it.polimi.ingsw.model;
+import java.util.ArrayList;
+
+/**
+ * @author Alessandra Atria
+ */
+
+    public class Depot {
+        private int dimension;
+        private ArrayList<Resource> resources;
+
+        public Depot(int dimension, ArrayList<Resource> resources) {
+            this.dimension = dimension;
+            this.resources = resources;
+        }
+
+    /**
+     * This method adds resources to the depot
+     * @param res represents the resource to add
+     */
+        public void addResource(Resource res){
+            int countRes;
+                countRes = this.resources.size();
+                if(countRes < dimension)
+                   this.resources.add(res);
+        }
+
+
+    /**
+     * This method removes resources from the depot
+     * @param res represents the resource to remove
+     */
+        public void removeResource(Resource res){
+               this.resources.remove(res);
+        }
+
+    /**
+     * This method gets the resources that are in the depot
+     */
+
+    public ArrayList<Resource> getDepot(){
+            return this.resources;
+        }
+
+    /**
+     * This method gets the maximum number of resources that the depot can contain
+     */
+        public int getDimension() {
+            return this.dimension;
+        }
+
+    /**
+     * This method checks if the depot doesn't have any resources
+     */
+        public boolean isEmpty(){
+          return  resources.isEmpty();
+        }
+
+    }
+
+
---
+++ src/test/java/it/polimi/ingsw/DepotTest.java
@@ -0,0 +1,117 @@
+package it.polimi.ingsw;
+import it.polimi.ingsw.enumerations.ResourceType;
+import it.polimi.ingsw.model.Depot;
+import it.polimi.ingsw.model.Resource;
+import org.junit.Before;
+import org.junit.Test;
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * @author Alessandra Atria
+ */
+
+public class DepotTest {
+
+    private static Depot depot1;
+    private static Depot depot2;
+
+    @Before
+    public void setUp() {
+        ArrayList<Resource> r1 = new ArrayList<>();
+        Resource r2 = new Resource(ResourceType.COIN);
+        Resource r3 = new Resource(ResourceType.SERVANT);
+        depot1 = new Depot( 3, r1);
+        depot2 = new Depot(2, r1);
+    }
+
+    /**
+     * checks if depot can get resources and its dimension
+     */
+    @Test
+    public void getterTest() {
+        ArrayList<Resource> resources = new ArrayList<>();
+        Resource r1 = new Resource(ResourceType.COIN);
+        Resource r2 = new Resource(ResourceType.SERVANT);
+        Resource r3 = new Resource(ResourceType.STONE);
+        resources.add(r1);
+        resources.add(r2);
+        resources.add(r3);
+        depot1 = new Depot( 1, resources);
+        depot2 = new Depot(2, resources);
+
+
+        assertEquals(depot2.getDepot(), resources);
+
+        assertEquals(depot1.getDimension(), 1);
+        assertEquals(depot2.getDimension(), 2);
+
+
+    }
+
+    /**
+     * checks if resources can be removed from depot
+     */
+    @Test
+    public void removeRes(){
+        int nRes;
+        ArrayList<Resource> resources = new ArrayList<>();
+        Resource r1 = new Resource(ResourceType.COIN);
+        Resource r2 = new Resource(ResourceType.SERVANT);
+        depot1 = new Depot( 3, resources);
+        resources.add(r1);
+        resources.add(r2);
+        nRes = resources.size();
+        assertTrue(nRes == 2);
+        depot1.removeResource(r1);
+        nRes = resources.size();
+        assertTrue(nRes == 1);
+        assertFalse(nRes == 2);
+        depot1.removeResource(r1);
+        nRes = resources.size();
+        assertFalse(nRes == 2);
+
+    }
+
+    /**
+     * checks if resources can be added to depot
+     */
+    @Test
+    public void addRes(){
+        int nRes;
+        ArrayList<Resource> resources = new ArrayList<>();
+        Resource r1 = new Resource(ResourceType.COIN);
+        Resource r2 = new Resource(ResourceType.SERVANT);
+        Resource r3 = new Resource(ResourceType.STONE);
+        Resource r4 = new Resource(ResourceType.SHIELD);
+        depot1 = new Depot( 3, resources);
+        depot1.addResource(r1);
+        depot1.addResource(r2);
+        depot1.addResource(r3);
+        nRes = resources.size();
+        assertTrue(nRes == 3);
+        depot1.addResource(r4);
+        nRes = resources.size();
+        assertFalse(nRes == 4);
+    }
+
+
+    /**
+     * checks if the depot is empty
+     */
+
+    @Test
+    public void isEmpty(){
+        ArrayList<Resource> resources = new ArrayList<>();
+        Resource r1 = new Resource(ResourceType.COIN);
+        depot1 = new Depot( 3, resources);
+        depot1.addResource(r1);
+        assertFalse(depot1.isEmpty());
+        depot1.removeResource(r1);
+        assertTrue(depot1.isEmpty());
+    }
+
+
+}
